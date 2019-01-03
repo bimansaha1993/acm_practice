235dd7fbac1c03944d4dce6f4a687800b1b47cce
= <=>
2019-01-03 19:06:46 +0600

    added till 3 january

---
+++ SortEstimate/SortEstimate/SortEstimate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+#define eps 1e-10
+int main()
+{
+	int c, time;
+	cin >> c >> time;
+	double low = 0, high = 100, mid;
+	while (abs(low - high)>eps)
+	{
+		mid = (low + high) / 2;
+		double a = c*mid*log(mid);
+		if (abs(a-time) <= eps)
+		{
+			cout << setprecision(15) << mid << endl;
+			break;
+		}
+		else if (a < time)
+			low = mid ;
+		else
+			high = mid ;
+
+	}
+	return 0;
+}
---
+++ autoloan/autoloan/autoloan.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+#define eps 1e-10
+int main()
+{
+	double price, payment, loan;
+	cin >> price >> payment >> loan;
+	double low = 0, high = 100, mid,mi,balance;
+	while (abs(low - high)>eps)
+	{
+		balance = price;
+		mid = (low + high) / 2;
+		mi = mid / 12;
+		for (int i = 0; i < loan; i++)
+		{
+			balance = balance + (balance*mi) / 100;
+			balance = balance - payment;
+		}
+		if (abs(balance) <= eps)
+		{
+			cout << setprecision(15) << mid << endl;
+			break;
+		}
+		else if (balance < 0)
+			low = mid;
+		else
+			high = mid;
+		
+	}
+	return 0;
+}
