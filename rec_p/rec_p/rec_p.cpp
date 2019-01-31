bc8338ec460da1f65efd7310bf5e0b28b3170f0c
= <=>
2019-01-31 18:45:39 +0600

    added till 31th january

--- rec_p/rec_p/rec_p.cpp
+++ rec_p/rec_p/rec_p.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 using namespace std;
-int n,a[100];
+int n,a[100],s[100];
 void rec(int i)
 {
 	if (i == n)
 	{  
+		cout << "{";
 		for (int j = 0; j < n; j++)
 		{
-			cout << a[j] << " ";
+			if (a[j] == 1){
+				cout << s[j]<<",";
+				//if (j < n - 1)
+					//cout << ",";
+			}
 		}
-		cout << endl;
+		cout << "}"<<endl;
 		return;
 	}
 	a[i] = 0;
@@ -21,6 +26,10 @@ void rec(int i)
 int main()
 {
 	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s[i];
+	}
 	rec(0);
 	return 0;
 }
