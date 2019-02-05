0332ce1e54fa1abbc721451fb884f8f179def918
= <=>
2019-02-05 19:51:59 +0600

    added till 5th feb

--- 4.UVA/divide_coins/divide_coins/divide_coins.cpp
+++ 4.UVA/divide_coins/divide_coins/divide_coins.cpp
@@ -1,24 +1,44 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-int n, m, a[101], coins[101];
-void rec(int i)
+#define size_sum 50001
+int n, m, a[101], coins[101], total, ans, dp[101][size_sum];
+int rec(int i,int sum1)
 {
+	int sum2, left, right;
 	if (i == m)
 	{
-		int sum = 0;
-		for (int j = 0; j < m; j++)
-		{
-			if (a[j] == 1)
-			{
-				sum = sum + coins[j];
-			}
-		}
-		cout << sum << endl;
+		int sum2 = total-sum1;
+		return dp[i][sum1]=abs(sum1 - sum2);
 	}
+	if (dp[i][sum1] != -1)
+		return dp[i][sum1];
 	a[i] = 0;
-	rec(i + 1);
+	left=rec(i + 1,sum1);
 	a[i] = 1;
-	rec(i + 1);
+	right=rec(i + 1,sum1+coins[i]);
+	if (left < right)
+		return dp[i][sum1]=left;
+	else
+		return dp[i][sum1]=right;
+}
+
+void reccase()
+{
+	total = 0;
+	for (int i = 0; i < m; i++)
+	{
+		total = total + coins[i];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < size_sum; j++)
+		{
+			dp[i][j] = -1;
+		}
+	}
+	ans = rec(0, 0);
+	cout << ans << endl;
 }
 
 int main()
@@ -31,7 +51,7 @@ int main()
 		{
 			cin >> coins[i];
 		}
-		rec(0);
+		reccase();
 	}
 	return 0;
 }
